7c9b59654c8bfe39178490a04e026ef201097dee
gcoayla <gcoayla.28@gmail.com>
2021-06-17 17:00:20 -0500

    Challenge completed

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // example interfaces that can be use
 // TIP: the types mentioned in the interfaces must be fulfilled in order to solve the problem.
 interface TemperatureReading {
@@ -13,14 +14,68 @@ interface TemperatureSummary {
   average: number
 }
 
+const sumarys: { [key: string]: { [item: string]: TemperatureSummary } } = {}
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  for (let read of readings) {
+    let exist = true
+    if (read.city in sumarys) {
+      if (!(read.time.toDateString() in sumarys[read.city])) {
+        exist = false
+      }
+    } else {
+      exist = false
+    }
+    if (!exist) {
+      const cityReadings = readings.filter(
+        (item) =>
+          item.city === read.city &&
+          item.time.toDateString() === read.time.toDateString(),
+      )
+      let cityFirst = cityReadings[0]
+      let cityLast = cityReadings[cityReadings.length - 1]
+      let cityHigh = cityReadings.reduce((a, b) =>
+        a.temperature > b.temperature ? a : b,
+      )
+      let cityLow = cityReadings.reduce((a, b) =>
+        a.temperature < b.temperature ? a : b,
+      )
+      let cityAvr = 0
+      for (let item of cityReadings) {
+        cityAvr += item.temperature
+      }
+      cityAvr = cityAvr / cityReadings.length
+      if (read.city in sumarys) {
+        sumarys[read.city][read.time.toDateString()] = {
+          first: cityFirst.temperature,
+          last: cityLast.temperature,
+          high: cityHigh.temperature,
+          low: cityLow.temperature,
+          average: cityAvr,
+        }
+      } else {
+        sumarys[read.city] = {
+          [read.time.toDateString()]: {
+            first: cityFirst.temperature,
+            last: cityLast.temperature,
+            high: cityHigh.temperature,
+            low: cityLow.temperature,
+            average: cityAvr,
+          },
+        }
+      }
+    }
+  }
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
+  if (city in sumarys) {
+    if (date.toDateString() in sumarys[city]) {
+      return sumarys[city][date.toDateString()]
+    }
+  }
   return null
 }
